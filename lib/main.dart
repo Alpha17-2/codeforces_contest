4cce872195b88135a933ab6aaa16c87d791d2768
Alpha17-2 <alphaisgod1@gmail.com>
2021-05-29 12:01:12 +0530

    added floating action button to update list

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:codeforces_contest/apiservices/cfapi.dart';
 import 'package:codeforces_contest/helpers/DeviceSize.dart';
-import 'package:codeforces_contest/helpers/loading.dart';
 import 'package:codeforces_contest/models/codeforcescontest.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:add_2_calendar/add_2_calendar.dart';
+
 void main(){
   runApp(MyApp());
 }
@@ -25,9 +25,18 @@ class home extends StatefulWidget {
 }
 class _homeState extends State<home> {
   @override
+  List<Result> upcomingContestList;
+  List<Result> ongoingContestList;
+
   void initState() {
     // TODO: implement initState
     super.initState();
+    getContestList();
+  }
+
+  void getContestList()async{
+    upcomingContestList = await codeforcesApiServices().fetchUpcomingContestList();
+    ongoingContestList = await codeforcesApiServices().fetchOngoingContestList();
   }
 
   @override
@@ -43,9 +52,9 @@ class _homeState extends State<home> {
       );
     }
 
-    Widget displayContest(List<Result> contest,int index){
+    Widget displayUpcomingContest(int index){
 
-      var date = DateTime.fromMillisecondsSinceEpoch(contest[index].startTimeSeconds * 1000);
+      var date = DateTime.fromMillisecondsSinceEpoch(upcomingContestList[index].startTimeSeconds * 1000);
       String start = DateFormat.yMMMd()
           .add_jm()
           .format(DateTime.parse(date.toString()));
@@ -68,7 +77,7 @@ class _homeState extends State<home> {
                   left: displayWidth(context)*0.03,
                   right: displayWidth(context)*0.35,
                   child: Text(
-                contest[index].name,
+                upcomingContestList[index].name,
                     overflow: TextOverflow.ellipsis,
                     style: TextStyle(
                       fontWeight: FontWeight.w500,
@@ -96,7 +105,72 @@ class _homeState extends State<home> {
                   icon: Icon(Icons.alarm_add),
                   onPressed: (){
                     Add2Calendar.addEvent2Cal(
-                      contestEvent(date, contest[index].name),
+                      contestEvent(date, upcomingContestList[index].name),
+                    );
+                  },
+                  color: Colors.green,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    Widget displayOngoingContest(int index){
+
+      var date = DateTime.fromMillisecondsSinceEpoch(ongoingContestList[index].startTimeSeconds * 1000);
+      String start = DateFormat.yMMMd()
+          .add_jm()
+          .format(DateTime.parse(date.toString()));
+      Color color = Color(0xfbf0f8ff);
+      return Container(
+        height: displayHeight(context)*0.1,
+        decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(25),
+            boxShadow: [
+              BoxShadow(color: Colors.black87,offset: Offset(-3,1),blurRadius: 2),
+            ]
+        ),
+        child: SafeArea(
+          child: Stack(
+            alignment: Alignment.centerRight,
+            children: [
+              Positioned(
+                  top: displayHeight(context)*0.02,
+                  left: displayWidth(context)*0.03,
+                  right: displayWidth(context)*0.35,
+                  child: Text(
+                    ongoingContestList[index].name,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: displayWidth(context)*0.0375,
+                      color: Colors.black,
+                    ),
+                  )),
+              Positioned(
+                  bottom : displayHeight(context)*0.03,
+                  left: displayWidth(context)*0.03,
+                  right: displayWidth(context)*0.1,
+                  child: Text(
+                    start,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w800,
+                      fontSize: displayWidth(context)*0.0375,
+                      color: Colors.black38,
+                    ),
+                  )),
+
+              Positioned(
+                right: displayWidth(context)*0.04,
+                child: IconButton(
+                  icon: Icon(Icons.alarm_add),
+                  onPressed: (){
+                    Add2Calendar.addEvent2Cal(
+                      contestEvent(date, ongoingContestList[index].name),
                     );
                   },
                   color: Colors.green,
@@ -166,45 +240,55 @@ class _homeState extends State<home> {
       body: TabBarView(
         children: [
           // Ongoing contest
-          Padding(padding: EdgeInsets.only(left: 8,right: 8,top: 20,bottom: 15)
-          ,child: FutureBuilder<List<Result>>(
-              future: codeforcesApiServices().fetchOngoingContestList(),
-              builder: (context,snapshots){
-                if(!snapshots.hasData)
-                  return Loading();
-                return ListView.builder(itemBuilder: (context,index){
-                  return Padding(
-                    padding: const EdgeInsets.only(bottom:18.0),
-                    child: displayContest(snapshots.data,index),
-                  );
-                },
-                  itemCount: snapshots.data.length,
+        Padding(
+              padding: const EdgeInsets.only(left: 8,right: 8,top: 20,bottom: 15),
+              child: ListView.builder(itemBuilder: (context,index){
+                return Padding(
+                  padding: const EdgeInsets.only(bottom:18.0),
+                  child: displayOngoingContest(index),
                 );
               },
+                itemCount: ongoingContestList.length,
+              ),
             ),
-          ),
 
           // Upcoming contests
           Padding(padding: EdgeInsets.only(left: 8,right: 8,top: 20,bottom: 15)
-            ,child: FutureBuilder<List<Result>>(
-              future: codeforcesApiServices().fetchUpcomingContestList(),
-              builder: (context,snapshots){
-                if(!snapshots.hasData)
-                  return Loading();
-                return ListView.builder(itemBuilder: (context,index){
-                  return Padding(
-                    padding: const EdgeInsets.only(bottom:18.0),
-                    child: displayContest(snapshots.data,index),
-                  );
-                },
-                  itemCount: snapshots.data.length,
-                );
-              },
-            ),
-          )
+              ,child: ListView.builder(itemBuilder: (context,index){
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom:18.0),
+                      child: displayUpcomingContest(index),
+                    );
+                  },
+                    itemCount: upcomingContestList.length,
+                  )
+              ),
         ],
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.api,color: Colors.white,size: displayWidth(context)*0.045,),
+            Text('Refresh',style: TextStyle(
+              fontSize: displayWidth(context)*0.024
+            ),)
+          ],
+        ) ,
+        onPressed: ()async{
+          upcomingContestList = await codeforcesApiServices().fetchUpcomingContestList();
+          ongoingContestList = await codeforcesApiServices().fetchOngoingContestList();
+          //  debugPrintStack(label: 'new list caught');
+          setState(() {
+          });
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:
+          Text('Contest list has been successfully updated !!'),duration: Duration(seconds: 4),));
+        },
+      ),
+    ),
+    );
+
   }
 }
 
